4051685339fe4642fa87e8213d088a3d8aa29755
Ben <kelsob@gmail.com>
2024-08-27 11:25:02 -0400

    Beginning sidebar styling adjustments.

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -3,7 +3,7 @@
 const Sidebar = () => {
   return (
     <aside className="w-1/4 bg-lightColor flex items-center justify-center fixed top-12 min-h-screen">
-      <nav className="space-y-4 text-center">
+      <nav className="space-y-4 text-center bg-mainColor">
         <a href="#home" className="block font-semibold text-darkColor hover:text-mainColor">Home</a>
         <a href="#portfolio" className="block font-semibold text-darkColor hover:text-mainColor">Portfolio</a>
         <a href="#contact" className="block font-semibold text-darkColor hover:text-mainColor">Contact</a>
