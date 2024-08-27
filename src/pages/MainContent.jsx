6cb4b3ba5f05dd674f30dfe9569cc25fdd43721c
kelsob <kelsob@gmail.com>
2024-08-27 16:00:20 -0400

    Fixed pages spilling over into consecutive pages at small screen sizes.

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -10,17 +10,17 @@ const MainContent = () => {
       <Sidebar />
 
       {/* Right Content Area */}
-      <div className="w-3/4 bg-white flex items-center justify-center ml-auto mt-12">
-        <div className="text-center w-full">
-          <section id="home" className="mb-16">
+      <div className="w-3/4 bg-white ml-auto mt-12">
+        <div className="flex flex-grow flex-col text-center w-full space-y-auto">
+          <section id="home" className="min-h-screen mb-16">
             <Home />
           </section>
 
-          <section id="portfolio" className="mb-16">
+          <section id="portfolio" className="min-h-screen mb-16">
             <Portfolio />
           </section>
 
-          <section id="contact">
+          <section id="contact" className="min-h-screen">
             <Contact />
           </section>
         </div>
