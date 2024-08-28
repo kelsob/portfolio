6a85bda67675c6b68bf3af8e5bf1ec41320426b7
Ben <kelsob@gmail.com>
2024-08-28 14:22:12 -0400

    Added dark mode css for sidebar.

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -26,32 +26,32 @@ const Sidebar = () => {
   }, []);
 
   return (
-    <aside className="w-1/4 bg-gradient-to-b from-lightColor to-superLightColor flex items-center justify-end pr-4 fixed min-h-screen border-r-4">
-      <div className="transition-all duration-500 ease-in-out shadow-xl rounded-lg border-y-4 border-x-2 border-Color md:translate-x-0 translate-x-4">
-        <nav className="transition-all duration-500 ease-in-out relative space-y-4 text-right bg-mainColor rounded-lg py-8 pl-8 pr-0 md:px-8">
-          <div className="transition-colors pr-5 border-r-4 border-mainColor md:border-lightColor rounded-lg relative">
+    <aside className="w-1/4 bg-gradient-to-b from-mainColor to-superLightColor dark:from-darkColor dark:to-lightColor flex items-center justify-end pr-4 fixed min-h-screen border-r-4 border-lightColor dark:border-superDarkColor">
+      <div className="transition-all duration-500 ease-in-out shadow-xl rounded-lg border-y-4 border-x-2 border-lightColor dark:border-mainColor md:translate-x-0 translate-x-2">
+        <nav className="transition-all duration-500 ease-in-out relative space-y-4 text-right bg-mainColor dark:bg-darkColor rounded-lg py-8 pl-8 pr-0 md:px-8">
+          <div className="transition-colors pr-6 border-mainColor relative">
             <a
               href="#home"
-              className={`block font-semibold ${activeSection === 'home' ? 'text-white hover:text-superLightColor' : 'text-superLightColor hover:text-white'}`}
+              className={`block font-semibold ${activeSection === 'home' ? 'text-white dark:text-superLightColor' : 'text-superLightColor hover:text-white dark:text-lightColor dark:hover:text-white'}`}
             >
               Home
             </a>
             <a
               href="#portfolio"
-              className={`block font-semibold ${activeSection === 'portfolio' ? 'text-white hover:text-superLightColor' : 'text-superLightColor hover:text-white'}`}
+              className={`block font-semibold ${activeSection === 'portfolio' ? 'text-white dark:text-superLightColor' : 'text-superLightColor hover:text-white dark:text-lightColor dark:hover:text-white'}`}
             >
               Portfolio
             </a>
             <a
               href="#contact"
-              className={`block font-semibold ${activeSection === 'contact' ? 'text-white hover:text-superLightColor' : 'text-superLightColor hover:text-white'}`}
+              className={`block font-semibold ${activeSection === 'contact' ? 'text-white dark:text-superLightColor' : 'text-superLightColor hover:text-white dark:text-lightColor dark:hover:text-white'}`}
             >
               Contact
             </a>
 
             {/* The animated dot */}
             <div
-              className={`absolute -translate-x-2 right-0 h-2 w-2 bg-white border-2 border-lightColor rounded-full transform transition-transform duration-300 ease-in-out ${
+              className={`absolute -translate-x-2 right-0 h-2 w-2 bg-white dark:bg-superLightColor rounded-full transform transition-transform duration-300 ease-in-out ${
                 activeSection === 'home' ? '-translate-y-4' :
                 activeSection === 'portfolio' ? 'translate-y-2' :
                 'translate-y-8'
